d80bbc5b459cf10698394c3ed6434633d1a55dbf
sajjad <sajadgholami.7040@gmail.com>
2021-07-14 11:54:21 +0430

    Fix Version 1

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,52 @@
 <template>
   <div id="app">
-    <b-excel />
+    <b-excel
+      :fetch="getItems"
+      header="excelHeader"
+      stringifyLongNum="stringifyLongNum"
+      :fields="fileds"
+      type="xls"
+      name="fileName"
+    >
+    <button>export exel</button>
+      <!-- can put your button hear for export exel -->
+    </b-excel>
   </div>
 </template>
 
 <script>
-import BExcel from './components/BExcel.vue'
+  import BExcel from "./components/BExcel.vue";
 
-export default {
-  name: 'App',
-  components: {
-    BExcel
+  export default {
+    name: "Export Excel",
+    components: {
+      BExcel,
+    },
+
+    data() {
+      return {
+        // thats data want to export excel
+        data: [
+          { name: "jun", age: 12 },
+          { name: "mohammad", age: 13 },
+          { name: "lucy", age: 20 }
+        ],
+        //create your json fileds
+        json_fileds: {
+          ["name"]: "name",
+          ["age"]: {
+            field: "age",
+            callback: (value) => {
+              return value
+            }
+          }
+        }
+      }
+    },
+    methods: {
+      getItems() {
+        return this.data
+      }
+    }
   }
-}
 </script>
