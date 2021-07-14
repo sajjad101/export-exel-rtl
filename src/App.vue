9312e0d546eebe322d94f69d402cd7bdfca142b1
sajjad <sajadgholami.7040@gmail.com>
2021-07-14 11:34:39 +0430

    Fist Releas

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <b-excel />
+  </div>
+</template>
+
+<script>
+import BExcel from './components/BExcel.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BExcel
+  }
+}
+</script>
